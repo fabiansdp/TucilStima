709b9c7a6f9480bb55b8c41a3ceca72477ee7a85
Fabian Pranoto <fabian.s.d.p@gmail.com>
2021-02-28 14:20:49 +0700

    add comments, test case, and docs folder

--- TopoSort/src/main.py
+++ TopoSort/src/main.py
@@ -1,4 +1,8 @@
+# Nama : Fabian Savero Diaz Pranoto
+# NIM : 13519140
+# Kelas : K03
 
+# Fungsi untuk inisialisasi graf berbentuk dictionary
 def initializeGraph(filename):
     f = open("test/" + filename)
 
@@ -6,42 +10,57 @@ def initializeGraph(filename):
     # key kode_kuliah dan value kuliah_prasyarat
     grafKuliah = dict()
 
+    # Baca setiap line, matkul pertama jadi key
+    # dan sisanya menjadi value berbentuk array
     for line in f.read().splitlines():
-        arrayKuliah = line.replace(".", "").split(",")
+        arrayKuliah = line.replace(".", "").replace(" ", "").split(",")
         grafKuliah[arrayKuliah[0]] = arrayKuliah[1:]
 
     f.close()
 
     return grafKuliah
 
-def recursiveSortHelper(seen, solution, graf, node):
-    for adjacent in graf[node]:
-        if adjacent not in seen:
-            seen.add(adjacent)
-            recursiveSortHelper(seen, solution, graf, adjacent)
+# Fungsi rekursif untuk pengecekan node di toposort
+def recursiveSortHelper(visited, solution, graf, node):
+    # Loop adjacency list di setiap node/key
+    # Jika node tidak memiliki node adjacent, tambah ke visited
+    # Jika memiliki adjacency list, loop dan apabila tidak ada di visited maka
+    # panggil fungsi rekursif untuk pengecekan node adjacent tersebut
+    # Jika node tidak ada di solusi, append ke list solution
+    if len(graf[node]) == 0:
+        visited.add(node)
+    else:
+        for adjacent in graf[node]:
+            if adjacent not in visited:
+                visited.add(adjacent)
+                recursiveSortHelper(visited, solution, graf, adjacent)
+
     if node not in solution:
         solution.append(node)
 
+# Fungsi toposort utama
 def topoSort(graf):
+    # Solution merupakan list hasil akhir toposort
+    # Visited merupakan sebuah set yang berisi node yang sudah
+    # dikunjungi
     solution = list()
-    seen = set()
+    visited = set()
 
+    # Loop setiap key di graf dan panggil fungsi rekursif
     for key in graf.keys():
-        recursiveSortHelper(seen, solution, graf, key)
+        recursiveSortHelper(visited, solution, graf, key)
 
     return solution
 
+# Fungsi untuk print solusi
 def printSolution(solution):
     print("\nRencana Kuliah:")
     for i in range(len(solution)):
         print(str(i+1)+".", solution[i])
 
 
-
+# Main Program
 filename = input("Masukkan nama berkas: ")
-
-grafKuliah = initializeGraph(filename)
-    
-solution = topoSort(grafKuliah)
-
-printSolution(solution)
+grafKuliah = initializeGraph(filename) # Inisialisasi graf
+solution = topoSort(grafKuliah) # list solusi
+printSolution(solution) # print solusi
