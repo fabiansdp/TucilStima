2b5bea38b413b98f9888afb45e6a03e7da08d4e4
Fabian Pranoto <fabian.s.d.p@gmail.com>
2021-02-28 20:28:39 +0700

    edit directory dan readme biar sesuai spek

--- TopoSort/README.md
+++ TopoSort/README.md
@@ -1,7 +1,14 @@
 # Tugas Kecil 2 IF2211 Strategi Algoritma
 
 ## Penjelasan Program
-Program python untuk melakukan Topological Sort yang merupakan penerapan algoritma Decrease and Conquer.
+Program Python untuk melakukan Topological Sort yang merupakan penerapan algoritma Decrease and Conquer. Algoritma yang dibuat mengecek simpul secara rekursif. Tahap pengecekan sebagai berikut:
+
+1. Mengecek simpul yang dimasukkan sebagai parameter.
+2. Mengiterasi setiap simpul tetangga yang ada di list tetangga:
+⋅⋅⋅Jika simpul tetangga belum pernah dikunjungi, masukkan ke dalam himpunan simpul lalu panggil fungsi recursiveSortHelper untuk mengecek simpul tersebut.
+⋅⋅⋅Jika pernah dikunjungi, lompati.
+3. Jika simpul yang dimasukkan sebagai parameter belum di dalam list solusi, masukkan simpul tersebut ke dalam list.
+
 
 ## Requirement
 - python 3.9.0
@@ -9,7 +16,10 @@ Program python untuk melakukan Topological Sort yang merupakan penerapan algorit
 ## Cara Menggunakan Program
 1. Buatlah file txt berisi daftar kuliah beserta kuliah prasyaratnya pada folder test. Contoh input sebagai berikut:
 ![alt text](photos/contohInput.png "Contoh File TXT")
-2. Jalankan program
+2. Jalankan program, dari directory root masukkan input sebagai berikut:
+```bash
+python bin/13519140.py
+```
 3. Input nama file txt yang akan hasil topological sortnya.
 4. Tunggu beberapa saat dan solusi sebagai berikut akan ditampilkan di command line.
 ![alt text](photos/contohOutput.png "Contoh Output")
---
+++ TopoSort/src/13519140.py
@@ -0,0 +1,65 @@
+# Nama : Fabian Savero Diaz Pranoto
+# NIM : 13519140
+# Kelas : K03
+
+# Fungsi untuk inisialisasi graf berbentuk dictionary
+def initializeGraph(filename):
+    f = open("test/" + filename)
+
+    # Inisialisasi dictionary dengan
+    # key kode_kuliah dan value kuliah_prasyarat
+    grafKuliah = dict()
+
+    # Baca setiap line, matkul pertama jadi key
+    # dan sisanya menjadi value berbentuk array
+    print("Isi Berkas:")
+    for line in f.read().splitlines():
+        print(line)
+        arrayKuliah = line.replace(".", "").replace(" ", "").split(",")
+        grafKuliah[arrayKuliah[0]] = arrayKuliah[1:]
+
+    f.close()
+
+    return grafKuliah
+
+# Fungsi rekursif untuk pengecekan node di toposort
+def recursiveSortHelper(visited, solution, graf, node):
+    # Loop adjacency list di setiap node/key
+    # Jika memiliki adjacency list, loop dan apabila tidak ada di visited maka
+    # panggil fungsi rekursif untuk pengecekan node adjacent tersebut
+    # Jika node tidak memiliki node adjacent, langsung cek apakah node ada di solusi
+    # Jika node tidak ada di solusi, append ke list solution
+    for adjacent in graf[node]:
+        if adjacent not in visited:
+            visited.add(adjacent)
+            recursiveSortHelper(visited, solution, graf, adjacent)
+
+    if node not in solution:
+        solution.append(node)
+
+# Fungsi toposort utama
+def topoSort(graf):
+    # Solution merupakan list hasil akhir toposort
+    # Visited merupakan sebuah set yang berisi node yang sudah
+    # dikunjungi
+    solution = list()
+    visited = set()
+
+    # Loop setiap key di graf dan panggil fungsi rekursif
+    for key in graf.keys():
+        recursiveSortHelper(visited, solution, graf, key)
+
+    return solution
+
+# Fungsi untuk print solusi
+def printSolution(solution):
+    print("\nRencana Kuliah:")
+    for i in range(len(solution)):
+        print(str(i+1)+".", solution[i])
+
+
+# Main Program
+filename = input("Masukkan nama berkas: ")
+grafKuliah = initializeGraph(filename) # Inisialisasi graf
+solution = topoSort(grafKuliah) # list solusi
+printSolution(solution) # print solusi
